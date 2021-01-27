a0b94632312102a2b52fc670352a9ee8574fd14c
raihankabir36850 <raihanku320@gmail.com>
2021-01-27 23:30:02 +0600

    birthday cake candles algorithm of hakerrank

--- easyAlgorithms.js
+++ easyAlgorithms.js
@@ -72,3 +72,31 @@ function plusMinus(arr) {
     const ratiozero = (zeroCount / arr.length).toPrecision(7)
     console.log(ratiozero)
 }
+
+
+
+//problem no:05
+//name:Birthday Cake Candles
+/*
+ * Complete the 'birthdayCakeCandles' function below.
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY candles as parameter.
+ */
+
+function birthdayCakeCandles(candles) {
+    // Write your code here
+    let tallestHeight = 0;
+    let count = 0;
+    for (let i = 0; i < candles.length; i++) {
+        if (candles[i] > tallestHeight) {
+            tallestHeight = candles[i]
+        }
+    }
+    for (let j = 0; j < candles.length; j++) {
+        if (candles[j] == tallestHeight) {
+            count++
+        }
+    }
+    return count;
+}
+
