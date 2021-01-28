9034403c15855ec5fbfe1cb99412774fd242fc98
raihankabir36850 <raihanku320@gmail.com>
2021-01-29 02:17:57 +0600

    piramid algorithms

--- easyAlgorithms.js
+++ easyAlgorithms.js
@@ -126,18 +126,41 @@ function miniMaxSum(arr) {
 }
 
 //problem no:07
-//name:find out power of base 10
+//name:find out 5 power of base 3
 //and it's concept of higher order function
 //complete the algorithm
 function base(b) {
     return function (n) {
         let result = 1;
-        for (let i = 0; i < n; i++) {
-            result *= b;
+        for (let i = 0; i < b; i++) {
+            result *= n;
         }
         return result;
     }
 }
-var power = base(2)
-var result = power(3)
+var power = base(3)
+var result = power(5)
 console.log(result)
+
+
+//problem no:08
+//name: piramid problem
+//complete the function
+function main(n) {
+    var midPoint = (2 * (n - 1)) / 2;
+    var columnIndex = (2 * n) - 1;
+    for (let row = 0; row < n; row++) {
+        let step = ''
+        for (column = 0; column < columnIndex; column++) {
+            if (midPoint - row <= column && midPoint + row >= column) {
+                step += 'H'
+            } else {
+                step += ' '
+            }
+        }
+        console.log(step)
+    }
+}
+main(10)
+
+
