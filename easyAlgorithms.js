b7c02bd1aecc535d0d769ff1c1ff666c65d644a0
raihankabir36850 <raihanku320@gmail.com>
2021-01-29 17:52:57 +0600

    grading students

--- easyAlgorithms.js
+++ easyAlgorithms.js
@@ -164,3 +164,23 @@ function main(n) {
 main(10)
 
 
+//problem no:9
+//name: grading students
+
+function gradingStudents(grades) {
+    // Write your code here
+    let grade = new Array(grades.length);
+    for (let x = 0; x < grades.length; x++) {
+        if (grades[x] >= 39 && (grades[x] + 1) % 5 === 0) {
+            grade[x] = grades[x] + 1
+        } else if (grades[x] >= 38 && (grades[x] + 2) % 5 === 0) {
+            grade[x] = grades[x] + 2
+        } else {
+            grade[x] = grades[x]
+        }
+    }
+    return grade;
+}
+const re = gradingStudents([84, 83, 68, 98, 100, 10, 15]);
+console.log(re);
+
