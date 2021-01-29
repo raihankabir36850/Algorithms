cfb0373bde1b15c9cd7a77f3c1d80046cf27c37c
raihankabir36850 <raihanku320@gmail.com>
2021-01-29 18:52:38 +0600

    apple and orrange count

--- easyAlgorithms.js
+++ easyAlgorithms.js
@@ -184,3 +184,24 @@ function gradingStudents(grades) {
 const re = gradingStudents([84, 83, 68, 98, 100, 10, 15]);
 console.log(re);
 
+
+
+//problem no:10
+//name:Apple and Orange
+// Complete the countApplesAndOranges function below.
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+    let appleCount = 0;
+    let orangeCount = 0;
+    for (let apple = 0; apple < apples.length; apple++) {
+        if (apples[apple] + a >= s && apples[apple] + a <= t) {
+            appleCount += 1;
+        }
+    }
+    for (let orange = 0; orange < oranges.length; orange++) {
+        if (oranges[orange] + b >= s && oranges[orange] + b <= t) {
+            orangeCount += 1;
+        }
+    }
+    console.log(`${appleCount}\n${orangeCount}`)
+}
+
