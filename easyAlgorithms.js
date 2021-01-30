61fd3968415471ca7b8aff2a5e465fac4bd8720d
raihankabir36850 <raihanku320@gmail.com>
2021-01-30 22:28:21 +0600

    Breaking the records

--- easyAlgorithms.js
+++ easyAlgorithms.js
@@ -220,3 +220,31 @@ function kangaroo(x1, v1, x2, v2) {
     }
 }
 
+
+//problem no:12
+//name:Breaking the records
+// Complete the breakingRecords function below.
+function breakingRecords(scores) {
+    let hightestScore = scores[0]
+    let lowestScore = scores[0]
+    let hightestCount = 0
+    let lowestCount = 0
+    let arr = new Array;
+    for (let i = 1; i < scores.length; i++) {
+        if (scores[i] > hightestScore) {
+            hightestScore = scores[i];
+            hightestCount += 1;
+        }
+        else if (scores[i] < lowestScore) {
+            lowestScore = scores[i];
+            lowestCount += 1
+        }
+    }
+    return [hightestCount, lowestCount];
+}
+
+
+
+
+
+
