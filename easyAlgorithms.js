37933645868d725eb716614fae996d4b06999ad4
raihankabir36850 <raihanku320@gmail.com>
2021-01-30 20:12:47 +0600

    number line jump

--- easyAlgorithms.js
+++ easyAlgorithms.js
@@ -205,3 +205,19 @@ function countApplesAndOranges(s, t, a, b, apples, oranges) {
     console.log(`${appleCount}\n${orangeCount}`)
 }
 
+//problem no:11
+//name:Number Line Jump
+// Complete the kangaroo function below.
+function kangaroo(x1, v1, x2, v2) {
+    if (v1 > v2) {
+        let remainder = (x2 - x1) % (v1 - v2)
+        if (remainder == 0) {
+            return 'YES'
+        } else {
+            return 'NO'
+        }
+    } else {
+        return 'NO'
+    }
+}
+
