f8e22724658380505393f7ee98606a430ecd0490
raihankabir36850 <raihanku320@gmail.com>
2021-02-06 00:29:16 +0600

    Day of the Programmer

--- easyAlgorithms.js
+++ easyAlgorithms.js
@@ -307,3 +307,35 @@ function birthday(s, d, m) {
 }
 
 
+//problem no:16
+//name:Day of the Programmer
+// Complete the day of the programer function below
+function dayOfProgrammer(year) {
+    if (year >= 1700 && year <= 1917) {
+        if (year % 4 == 0) {
+            let dayCount = 12;
+            let monthCount = 9;
+            return `${dayCount}.0${monthCount}.${year}`
+
+        } else {
+            let dayCount = 13;
+            let monthCount = 9;
+            return `${dayCount}.0${monthCount}.${year}`
+        }
+    } else if (year == 1918) {
+        let dayCount = 26;
+        let monthCount = 9;
+        return `${dayCount}.0${monthCount}.${year}`
+    }
+    else if (year >= 1919 && year <= 2700) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            let dayCount = 12;
+            let monthCount = 9;
+            return `${dayCount}.0${monthCount}.${year}`
+        } else {
+            let dayCount = 13;
+            let monthCount = 9;
+            return `${dayCount}.0${monthCount}.${year}`
+        }
+    }
+}
