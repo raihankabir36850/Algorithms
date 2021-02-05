7c9d282d1acbad89db5a5523ffa2b2d9ec0a61f0
raihankabir36850 <raihanku320@gmail.com>
2021-02-05 19:18:24 +0600

    subarray division array

--- easyAlgorithms.js
+++ easyAlgorithms.js
@@ -290,6 +290,20 @@ function migratoryBirds(arr) {
 const su = migratoryBirds([1, 3, 1, 2]);
 
 
-
+//problem no:15
+//name:subarray division
+//// Complete the subarray division function below.
+function birthday(s, d, m) {
+    let count = 0;
+    for (let i = 0; i <= s.length - m; i++) {
+        let sum = 0;
+        for (let j = i; j < i + m; j++) {
+            sum += s[j];
+        }
+        if (sum == d)
+            count += 1;
+    }
+    return count;
+}
 
 
